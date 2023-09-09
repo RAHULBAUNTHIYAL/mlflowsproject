5ad2c8a47698fdc3ed64480db51de17cc714354f
rahul baunthiyal <raahulbaunthiyal48@gmail.com>
2023-09-09 22:25:16 +0530

    Your commit message here

--- src/mlproject/utils/common.py
+++ src/mlproject/utils/common.py
@@ -5,9 +5,10 @@ from mlproject import logger
 import json
 import joblib
 from ensure import ensure_annotations
-from box import ConfigBox
+from box.config_box import ConfigBox
 from pathlib import Path
 from typing import Any
+
 
 
 
@@ -29,6 +30,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
             logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            
             return ConfigBox(content)
     except BoxValueError:
         raise ValueError("yaml file is empty")
@@ -49,6 +51,7 @@ def create_directories(path_to_directories: list, verbose=True):
         os.makedirs(path, exist_ok=True)
         if verbose:
             logger.info(f"created directory at: {path}")
+            
 
 
 @ensure_annotations
